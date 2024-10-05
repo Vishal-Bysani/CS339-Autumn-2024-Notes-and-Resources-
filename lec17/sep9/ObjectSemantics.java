e4efa0c237986b415e39ae078f11fbddf219e940
Vishal-Bysani <vishal.bysani99@gmail.com>
2024-10-05 19:52:33 +0530

    Added new slides

--- lec17/sep9/ObjectSemantics.java
+++ lec17/sep9/ObjectSemantics.java
@@ -1,14 +1,9 @@
 public class ObjectSemantics {
     public static void main(String[] args) {
-        Animal p1;
-//        p1 = new Animal();
-//        System.out.println(p1.x + " " + p1.y);
         Animal p2;
         p2 = new Monkey();
-//        System.out.println(p2.x + " " + p2.y + " " + ((Monkey) p2).y + " " + ((Monkey) p2).z);
-        p2.sound();
-        p2.tail();
-        ((Animal) p2).tail();
+        System.out.println(p2.x + " " + p2.y + " " + ((Monkey) p2).y + " " + ((Monkey) p2).z);
+
     }
 }
 
